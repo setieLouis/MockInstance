928c82446d01bb83b9b204f0eee9364c8f98a12a
SETIE LOUIS ADJAMBRI <setielouis.adjambri@ext.illimity.com>
2020-11-16 22:48:19 +0100

    INIT COMMIT

---
+++ src/main/java/com/example/TestStaticIo/TestStaticIoApplication.java
@@ -0,0 +1,13 @@
+package com.example.TestStaticIo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestStaticIoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestStaticIoApplication.class, args);
+	}
+
+}
