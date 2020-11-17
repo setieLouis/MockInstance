7845fb55ec81f2c5afea0b409e140254e6977186
SETIE LOUIS ADJAMBRI <setielouis.adjambri@ext.illimity.com>
2020-11-17 23:29:29 +0100

    [GREEN] Handle not foundException

--- src/test/java/com/example/TestStaticIo/JsonToObjectConverterTest.java
+++ src/test/java/com/example/TestStaticIo/JsonToObjectConverterTest.java
@@ -4,14 +4,12 @@ import lombok.Data;
 import org.junit.Before;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 
 public class JsonToObjectConverterTest {
@@ -27,7 +25,17 @@ public class JsonToObjectConverterTest {
     @Test
     public void readAndConvertTest() throws IOException {
 
-        TestModel model = jsonToObjectConverter.convert("json/TestMode.json", TestModel.class);
+        assertThatExceptionOfType(FileNotFoundException.class)
+                .isThrownBy( () ->
+                jsonToObjectConverter.convert("wrongPath", TestModel.class)
+                 )
+                .withMessage("class path resource [wrongPath] cannot be resolved to absolute file path because it does not exist");
+    }
+
+    @Test
+    public void readAndConvesrtTest() throws IOException {
+
+        TestModel model = jsonToObjectConverter.convert("json/TestMo.json", TestModel.class);
 
         assertThat(model.attr1).isEqualTo("value1");
         assertThat(model.attr2).isEqualTo("value2");
